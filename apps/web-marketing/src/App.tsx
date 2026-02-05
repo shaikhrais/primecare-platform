2c54d0a75bee8d3b687b05a893ef8f5ba3e451b2
Shaikh M Rais <shkh.rais@gmail.com>
2026-02-04 22:21:17 -0500

    feat: implemented ScrollToTop component to reset scroll on route change

--- apps/web-marketing/src/App.tsx
+++ apps/web-marketing/src/App.tsx
@@ -2,11 +2,13 @@ import React, { Suspense } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { useRoutes } from 'react-router-dom';
 import { routes } from './routes';
+import ScrollToTop from './components/layout/ScrollToTop';
 
 function App() {
     const element = useRoutes(routes);
     return (
         <HelmetProvider>
+            <ScrollToTop />
             <Suspense fallback={<div>Loading...</div>}>
                 {element}
             </Suspense>
